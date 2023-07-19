11345f69f5e055a76b32b628954f9eaa6fa15d72
arjunghimire <arjun.ghimire@devfinity.io>
2023-07-19 13:37:24 +0545

    Project

---
+++ QRScanner/Controllers/QRScannerController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using QRScanner.Models;
+using System.Text.Json;
+
+namespace QRScanner.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QRScannerController : ControllerBase
+    {
+        [HttpPost("readQR")]
+        public async Task<ActionResult> ReadQRScan()
+        {
+            return StatusCode(200);
+        }
+
+        [HttpPost("generateQR")]
+        public async Task<ActionResult> GenerateQR()
+        {
+            var accountDetail = new Contact
+            {
+                Id = Guid.NewGuid(),
+                FirstName="Arjun",
+                LastName="Ghimire",
+                Phone="+977-9867278173",
+                Email="developerarjun1@gmail.com",
+                ImageUrl=string.Format("https://i.stack.imgur.com/2meqv.jpg?s=256&g=1"),
+                CompanyName="Devfinity LLC",
+                Website=string.Format("https://stackoverflow.com/users/12647344/arjun-ghimire"),
+                BioDescription="Software Engineer",
+            };
+            //ECCLevel Error Correction Level 
+            /*
+             * Choosing a higher ECL will ensure that the QR code remains readable even after taking damage, 
+             * but will also increase the size of the QR code. With each increment in the ECl, 
+             * more data modules are added which contain the backed-up data; thus increasing the QR code size.
+            */
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(JsonSerializer.Serialize(accountDetail), QRCodeGenerator.ECCLevel.H);
+            PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+            byte[] qrCodeAsPngByteArr = qrCode.GetGraphic(20);
+            string base64ImageRepresentation = "data:image/png;base64,"+Convert.ToBase64String(qrCodeAsPngByteArr);
+            return StatusCode(200, base64ImageRepresentation);
+        }
+    }
+}
---
+++ QRScanner/Models/Contact.cs
@@ -0,0 +1,53 @@
+﻿namespace QRScanner.Models
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        //public string Bio { get; set; }
+        public string ImageUrl { get; set; }
+        public string SmallImageUrl { get; set; }
+        public string CompanyName { get; set; }
+        public string Website { get; set; }
+        public string BioDescription { get; set; }
+        public virtual ICollection<ContactDetails> ContactDetails { get; set; }
+    }
+    public class ContactDetails
+    {
+        public Guid Id { get; set; }
+        public string LinkedInUrl { get; set; }
+        public string FacebookUrl { get; set; }
+        public string InstagramUrl { get; set; }
+        public string TwitterUrl { get; set; }
+        public string TiktokUrl { get; set; }
+        public string SnapChatUrl { get; set; }
+        public string TwitchUrl { get; set; }
+        public string PinterestUrl { get; set; }
+        public string VimeoUrl { get; set; }
+        public string YouTubeUrl { get; set; }
+        public string SpotifyUrl { get; set; }
+        public string SoundCloudUrl { get; set; }
+        public string AppleMusicUrl { get; set; }
+        public string ITunesUrl { get; set; }
+        public string YouTubeMusicUrl { get; set; }
+        public string AmazonMusicUrl { get; set; }
+        public string CalendlyUrl { get; set; }
+        public string NotionUrl { get; set; }
+        public string GoogleDriveUrl { get; set; }
+        public string DribbleUrl { get; set; }
+        public string GitHubUrl { get; set; }
+        public string BehanceUrl { get; set; }
+        public string EtsyUrl { get; set; }
+        public string CashAppUrl { get; set; }
+        public string VenmoUrl { get; set; }
+        public string PaypalUrl { get; set; }
+        public string WhatsappUrl { get; set; }
+        public string DiscordUrl { get; set; }
+        public string WechatUrl { get; set; }
+        public string TelegramUrl { get; set; }
+        public string SignalUrl { get; set; }
+    }
+}
